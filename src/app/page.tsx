91ec9a66030d7dfec9f7fcda7cfbce2415a3f8d2
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-30 15:16:31 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/base/Navbar";
 import HeroSection from "@/components/base/HeroSection";
-import Footer from "@/components/base/Footer";
 import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
 export default async function LandingPage() {
@@ -9,7 +8,6 @@ export default async function LandingPage() {
     <div className="min-h-screen flex flex-col ">
       <Navbar user={session?.user ?? null} />
       <HeroSection />
-      <Footer />
     </div>
   );
 }
